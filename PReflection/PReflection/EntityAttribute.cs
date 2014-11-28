465c2b3a6f6a7778b4aa537186b174c1827b9653
Pablo Sánchez <pablosanchezflores@gmail.com>
2014-11-28 11:38:56 +0100

    Actualizando PReflection

---
+++ PReflection/PReflection/EntityAttribute.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PReflection
+{
+	public class EntityAttribute
+	{
+		public EntityAttribute ()
+		{
+		}
+	}
+}
+
---
+++ PReflection/PReflection/ValidationAttribute.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace PReflection
+{
+	public class ValidationAttribute
+	{
+		public ValidationAttribute ()
+		{
+		}
+	}
+}
+
