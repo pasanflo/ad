88522031b8168c061770589a0d0f1769ceec56e6
Pablo Sánchez <pablosanchezflores@gmail.com>
2014-12-05 12:30:20 +0100

    Subiendo progreso

--- JArticulo/src/Serpis/Ad/Articulo.java
+++ JArticulo/src/Serpis/Ad/Articulo.java
@@ -9,24 +9,23 @@ import java.util.*;
 import java.sql.PreparedStatement;
 public class Articulo {
 	private static Scanner scanner=new Scanner(System.in);
-	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+	public static void main(String[] args) throws SQLException {
 		// TODO Menú con herramientas de edición
 		System.out.println("Menu:");
 		System.out.println("0-salir/1-nuevo/2-editar/3-eliminar/4-ver");
 
 		//Class.forName("com.mysql.jdbc.Driver");
 		Connection connection=DriverManager.getConnection("jdbc:mysql://localhost/dbprueba?user=root&password=sistemas");
-		//Statement statement=connection.createStatement();
+		Statement statement=connection.createStatement();
 		//Sin parametros
-		//ResultSet resultset=statement.executeQuery("select * from categoria");
+		ResultSet resultset=statement.executeQuery("select * from categoria");
 		//Con parametros
-		PreparedStatement preparedStatement=connection.prepareStatement("select * from categoria where id=?");
-		preparedStatement.setLong(1,19);//en el primer interrogante de id busca el 19
-		ResultSet resultSet=preparedStatement.executeQuery();
-		while(resultSet.next()){
+		//PreparedStatement preparedStatement=connection.prepareStatement("select * from categoria where id=?");
+		//preparedStatement.setLong(1,20);
+		//ResultSet resultSet=preparedStatement.executeQuery();
+		while(resultSet.next())
 			System.out.printf("id=%4s nombre=%s\n",resultSet.getObject("id"),resultSet.getObject("nombre"));
-			resultSet.getObject("nombre");
-		}
+
 		resultSet.close();
 		connection.close();
 	}
