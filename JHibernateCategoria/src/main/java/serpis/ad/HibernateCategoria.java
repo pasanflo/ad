e7ea63f3b450ec5ecfe848ecdc152b4757beab65
Pablo Sánchez <pablosanchezflores@gmail.com>
2015-01-19 10:35:12 +0100

    Listando Categoria con Hibernate

--- JHibernateCategoria/src/main/java/serpis/ad/HibernateCategoria.java
+++ JHibernateCategoria/src/main/java/serpis/ad/HibernateCategoria.java
@@ -1,5 +1,9 @@
 package serpis.ad;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -9,12 +13,43 @@ public class HibernateCategoria {
 	
 	//Los métodos setup y teardown se ejecutan justo antes y después de los métodos de test.
 	//Debemos introducir la creación del entityManagerFactory en el setup y el close() en teardown.
+	
+	private static EntityManagerFactory entityManagerFactory;
 
 	public static void main(String[] args) {
-		EntityManagerFactory entityManagerFactory = 
-				Persistence.createEntityManagerFactory("serpis.ad.hibernate.mysql");
+		
+		entityManagerFactory = Persistence.createEntityManagerFactory("serpis.ad.hibernate.mysql");
+		
+		showCategorias();
+		System.out.println("Añadiendo categoria...");
+		persistNuevasCategorias();
+		
 		entityManagerFactory.close();
 
+	}
+	public static void persistNuevasCategorias(){
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		
+		Categoria categoria = new Categoria();
+		categoria.setNombre("Hibernate -> " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()));
+		
+		entityManager.persist(categoria);
+		
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+	
+	public static void showCategorias(){
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		
+		List<Categoria> categorias = entityManager.createQuery("FROM Categoria", Categoria.class).getResultList();
+		for (Categoria categoria : categorias)
+			System.out.printf("id=%d nombre=%s\n", categoria.getId(), categoria.getNombre());
+		
+		entityManager.getTransaction().commit();
+		entityManager.close();
 	}
 
 }
